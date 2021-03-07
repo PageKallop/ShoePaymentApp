7baba1abeab2be64a1e8cdb5e1306782007f6215
PageKallop <PageMarie78@gmail.com>
2021-03-06 18:50:44 -0600

    First commit

--- ShoePaymentApp/ViewController.swift
+++ ShoePaymentApp/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    struct Shoes {
+        var name: String
+        var price: Double 
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    //displays an alert 
+    func displayDefaultAlert(title: String?, message: String?) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
     }
 
 
