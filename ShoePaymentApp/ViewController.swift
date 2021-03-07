c3e92f27a919bc18c9304cc539247d8335fd15db
PageKallop <PageMarie78@gmail.com>
2021-03-06 19:22:34 -0600

    Add shoe array Design UI

--- ShoePaymentApp/ViewController.swift
+++ ShoePaymentApp/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UIViewController {
         var name: String
         var price: Double 
     }
+    
+    let shoeData = [
+        Shoes(name: "Nike Air Force 1 High LV8", price: 120.00),
+        Shoes(name: "adidas Super Stars", price: 135.00),
+        Shoes(name: "Jordan Retro 10", price: 190.00),
+        Shoes(name: "adidas Original Prophere", price: 75.00),
+        Shoes(name: "New Balance 574", price: 90.00)
+    ]
 
+    @IBOutlet weak var shoePickerView: UIPickerView!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func buyButton(_ sender: UIButton) {
+        print("pressed")
+    }
     
+ 
     //displays an alert 
     func displayDefaultAlert(title: String?, message: String?) {
         
